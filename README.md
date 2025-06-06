866d2389ad94996e5d20c0d255931b5662b6c4d6
shaexintran <shaexin@icloud.com>
2025-06-06 14:43:52 -0700

    create and delete added, new edit.ejs

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# book-shelf
+# Bookshelf
+A web app to collect and manage your books with Model-View-Controller (MVC) architecture.
+
+## Tech Stack
+- JavaScript
+- Node.js
+- Express.js
+- EJS (Embedded JavaScript)
+- MongoDB + Mongoose
+
+## Requirements
+- Create a full-stack application using Node.js, Express, EJS, and MongoDB.
+- The application will center around a resource or schema of your choosing with full Create, Read, Update, and Delete (CRUD) functionalities.
+- Define how this data will be structured in a Mongoose Schema. You need to consider what properties your resource will have and what data types these properties will be.
+
+Your completed application should have the following RESTful routes:
+
+| HTTP Method |      Route       |  Action |             Description                |
+| ----------- | ---------------- | ------- | -------------------------------------- |
+| GET         | /plants          | Index   | Displays a list of all plants          |
+| GET         | /plants/new      | New     | Shows a form to create a new plant     |
+| POST        | /plants          | Create  | Creates a new plant                    |
+| GET         | /plants/:id      | Show    | Displays a specific plant by its ID    |
+| GET         | /plants/:id/edit | Edit    | Shows a form to edit an existing plant |
+| PUT         | /plants/:id      | Update  | Updates a specific plant by its ID     |
+| DELETE      | /plants/:id      | Destroy | Deletes a specific plant by its ID     |
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const express = require('express')
 const path = require("path")
 const mongoose = require('mongoose')
 const Book = require('./models/books.js')
+const methodOverride = require("method-override"); // new
+const morgan = require("morgan"); //new
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -17,6 +21,14 @@ app.get('/', (req, res) => {
 app.get('/books', async (req, res) => {
   const allBooks = await Book.find()
   res.render('books/index', { books: allBooks })
+})
+
+app.post('/books', async (req, res) => {
+  // this will be where we get the data from the post form
+  req.body.isOwned = req.body.isOwned === 'on' ? true : false
+  const newBook = await Book.create(req.body)
+  console.log(newBook)
+  res.redirect('/books')
 })
 
 app.get('/books/new', (req, res) => {
@@ -28,11 +40,21 @@ app.get('/books/:bookId', async (req, res) => {
   res.render('books/show', { book: foundBook })
 })
 
-app.post('/books', async (req, res) => {
-  // this will be where we get the data from the post form
+app.get('/books/:bookId/edit', async (req, res) => {
+  const foundBook = await Book.findById(req.params.bookId)
+  res.render('books/edit', { book: foundBook })
+})
+
+app.put('/books/:bookId', async (req, res) => {
   req.body.isOwned = req.body.isOwned === 'on' ? true : false
-  const newBook = await Book.create(req.body)
-  console.log(newBook)
+  // update the fruit
+  await Book.findByIdAndUpdate(req.params.bookId, req.body)
+  res.redirect(`/books/${req.params.bookId}`)
+})
+
+app.delete('/books/:bookId', async (req, res) => {
+  // ! ADD ADDITIONAL CHECK to avoid accidental deletions
+  await Book.findByIdAndDelete(req.params.bookId)
   res.redirect('/books')
 })
 
