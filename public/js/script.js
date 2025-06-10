81fe677b95b0fa0b36b2d70123e749de9fd22908
shaexintran <shaexin@icloud.com>
2025-06-10 12:48:38 -0700

    internal search bar included in bookshelf pages

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,11 @@
 const searchInput = document.querySelectorAll('.search-shelf')
-
-// hide bookListElements if doesn't match
 const bookListElements = document.querySelectorAll('li.book')
 
-// Add an event listener to search input that logs the value of whatever is typed
 searchInput.forEach(search => {
     search.addEventListener('input', e => {
         const value = e.target.value.toLowerCase()
         console.log(value)
         bookListElements.forEach(book => {
-            // sets to true or false if includes value
             const isVisible = book.textContent.toLowerCase().includes(value)
             book.classList.toggle('hidden', !isVisible)
         })
